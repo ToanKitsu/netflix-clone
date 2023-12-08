ff4d3d2fd6f85274360de06f704ecc6f0d0f717a
ToanKitsu <nguyenngoctoanrc@gmail.com>
2023-12-08 17:15:51 +0700

    fix SavedShows

--- src/components/SavedShows.jsx
+++ src/components/SavedShows.jsx
@@ -3,17 +3,29 @@ import { useState, useEffect } from "react";
 import { UserAuth } from "../context/AuthContext";
 import { db } from "../firebase";
 import { updateDoc, doc, onSnapshot } from "firebase/firestore";
+import { useTask } from "../context/context";
+import { useNavigate } from "react-router-dom";
 
 const SavedShows = () => {
   const [movies, setMovies] = useState([]);
   const { user } = UserAuth();
+  const { truncateString } = useTask();
+  const navigate = useNavigate();
+
+  const handleIconClick = (e) => {
+    e.stopPropagation();
+  };
+
+  const handleMoreInfo = (id) => {
+    navigate(`/movies/${id}`);
+  };
 
   const slideLeft = () => {
-    var slider = document.getElementById("slider");
+    const slider = document.getElementById("slider");
     slider.scrollLeft = slider.scrollLeft - 500;
   };
   const slideRight = () => {
-    var slider = document.getElementById("slider");
+    const slider = document.getElementById("slider");
     slider.scrollLeft = slider.scrollLeft + 500;
   };
 
@@ -26,14 +38,6 @@ const SavedShows = () => {
       });
     } catch (error) {
       console.log(error);
-    }
-  };
-
-  const truncateString = (str, num) => {
-    if (str?.length > num) {
-      return str.slice(0, num) + "...";
-    } else {
-      return str;
     }
   };
 
@@ -64,21 +68,24 @@ const SavedShows = () => {
               <div
                 key={id}
                 className="hover:scale-125 duration-500 hover:z-20 w-[160px] sm:w-[200px] md:w-[240px] lg:w-[280px] inline-block cursor-pointer relative p-2"
+                onClick={() => handleMoreInfo(item?.id)}
               >
                 <img
-                  src={`https://image.tmdb.org/t/p/w500/${item.img}`}
-                  alt={item.title}
+                  src={`https://image.tmdb.org/t/p/w500/${item?.img}`}
+                  alt={item?.title}
                 />
                 <div className="absolute top-0 left-0 w-full h-full hover:bg-black/80 opacity-0 hover:opacity-100 text-white hover:scale-[95%]">
                   <p className="white-space-normal text-xs md:text-sm font-bold flex justify-center items-center h-full w-full">
                     {truncateString(item.title, 30)}
                   </p>
-                  <p
-                    onClick={() => deleteShow(item.id)}
-                    className="absolute top-0 right-0 p-3 text-gray-300"
-                  >
-                    <AiOutlineClose />
-                  </p>
+                  <div onClick={handleIconClick}>
+                    <p
+                      onClick={() => deleteShow(item?.id)}
+                      className="absolute top-0 right-0 p-3 text-gray-300"
+                    >
+                      <AiOutlineClose />
+                    </p>
+                  </div>
                 </div>
               </div>
             );
